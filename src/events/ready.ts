818bc03aa7520bc9f65ff665d026524301c75646
Greensky-gs <72513011+Greensky-gs@users.noreply.github.com>
2023-02-12 07:04:00 +0100

    feat(struct): :sparkles: Implement Group Data manager in client

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,9 +1,17 @@
 import { client } from '..';
 import { Event } from '../structures/Event';
+import { groupDataManager } from '../structures/groupDataManager';
 import { checkDatabase } from '../utils/database';
 
-export default new Event('connected', () => {
+export default new Event('connected', async () => {
     console.log(`Logged as ${client.user.username}`);
 
-    checkDatabase().catch(console.log);
+    await checkDatabase().catch(console.log);
+    client.groupDatas = new groupDataManager();
 });
+
+declare module '@androz2091/insta.js' {
+    interface Client {
+        groupDatas: groupDataManager;
+    }
+}
