856d76f2be3efba2b8343e3ca14f0e9ce1324672
Greensky <david.hesliere@gmail.com>
2023-02-16 17:48:06 +0100

    feat(cmd): :sparkles: Add jokes

--- src/commands/blague.ts
+++ src/commands/blague.ts
@@ -3,10 +3,16 @@ import { Command } from '../structures/Command';
 export default new Command({
     name: 'blague',
     description: 'Envoie une blague'
-}).setRun(async (message) => {
-    const joke = await message.client.handler.container.jokes.randomCategorized('global').catch(() => {});
+}).setRun(async (message, args) => {
+    const Categories = message.client.handler.container.jokes.categories;
+    let type = Categories.GLOBAL;
+    if (/^noire?/i.test(args.join(' '))) type = Categories.DARK;
+    if (/^limite?/i.test(args.join(' '))) type = Categories.LIMIT;
+
+    const joke = await message.client.handler.container.jokes.randomCategorized(type).catch(() => {});
 
     if (!joke) return message.chat.sendMessage(`J'ai plus de blagues à raconter`, null).catch(() => {});
+    console.log(joke);
 
     await message.chat.sendMessage(joke.joke, null).catch(() => {});
     setTimeout(() => {
